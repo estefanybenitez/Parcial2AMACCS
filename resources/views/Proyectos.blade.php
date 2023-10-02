3215b26a0d8aba7279498af27dffd0e6668c6246
Erickson <erikson.polanco94@gmail.com>
2023-10-02 01:13:09 -0600

    Se agrega pagina de Proyectos

--- resources/views/Proyectos.blade.php
+++ resources/views/Proyectos.blade.php
@@ -1,10 +1,78 @@
 @extends('Layout.app')
 
 @section('content')
+<link rel="stylesheet" href="./proyectos.css">
+
 <h1 class="text-center">Proyectos</h1>
 <hr>
-
-{{-- insertar contenido  --}}
+<!--Erikson -->
+<section class="bg-image">
+    <div class="center-content">
+        <h1>Conoce nuestros proyectos</h1>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+        <a href="#" class="btn btn-primary">Más información</a>
+    </div>
+</section>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src=".\img-proyectos\concepto-ciudad-inteligente-futurista.webp" alt="Proyecto 1" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Proyecto: Ciudad futurista</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src=".\img-proyectos\concepto-programacion.webp" alt="Proyecto 2" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Proyecto: Diseño Web</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src=".\img-proyectos\hombre-gafas-vr.webp" alt="Proyecto 3" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Proyecto: Realidad Virtual al alcance de todos</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src=".\img-proyectos\mano-sujetando-pantalla.webp" alt="Proyecto 1" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Proyecto: Tecnologia en la industria</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src=".\img-proyectos\retrato-negocios.webp" alt="Proyecto 2" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Proyecto: Establecimiento de contratos</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img src=".\img-proyectos\trabajador-funcionamiento-maquina-industrial.webp" alt="Proyecto 3" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Proyecto: Maquinaria industrial</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
