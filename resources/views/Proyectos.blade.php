7712e85bfb2283c4d466e22ad96ba844872f6edb
carito16 <114324587+carito16@users.noreply.github.com>
2023-10-01 22:09:19 -0600

    Sección Proyectos
    Contenido agregado a la sección de proyectos

--- resources/views/Proyectos.blade.php
+++ resources/views/Proyectos.blade.php
@@ -6,6 +6,26 @@
 
 {{-- insertar contenido  --}}
 
+<p style="padding: 15px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio id,
+    dignissimos natus et exercitationem praesentium perspiciatis officia
+    aperiam blanditiis voluptates.
+</p>
 
+<div style="text-align: center;">
+    <div style="display: inline-block; margin: 10px; text-align: center;">
+        <img src="{{ asset('images/App.jpg') }}" style="width: 300px;">
+        <h4>Apps</h4>
+    </div>
+
+    <div style="display: inline-block; margin: 10px; text-align: center;">
+        <img src="{{ asset('images/Desktop.jpg') }}" style="width: 260px;">
+        <h4>Escritorio</h4>
+    </div>
+
+    <div style="display: inline-block; margin: 10px; text-align: center;">
+        <img src="{{ asset('images/Web.jpg') }}" style="width: 300px;">
+        <h4>Web</h4>
+    </div>
+</div>
 
 @endsection
