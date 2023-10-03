7b23f5de44d85a6b4ed6c2a184d00ccf2640b05e
MINEDUCYT <estefany.benitez22@itca.ed.sv>
2023-10-03 07:59:06 -0600

    editado  home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -2,12 +2,56 @@
 
 @section('content')
 <h1 class="text-center">Home</h1>
-<hr>
+{{-- <hr> --}}
 {{-- insertar contenido  --}}
+<img src="./Agencia2.jpg" alt="agencia" width="100%" height="350px">
 
+<div class="row align-items-start p-3 m-2">
+  <h2> Sobre Nosotros </h2>
+  <p>es una empresa de consultoría y administración de viajes que brinda una completa respuesta a las necesidades de empresas y ejecutivos para viajes de negocios y eventos corporativos.
+    Pertenecemos a la red más sólida y reconocida de agencias de viajes de Latinoamérica L’alianxa Travel Network, con más de 250 puntos de ventas en 95 ciudades, en 18 países. Además, somos parte de la 
+    presitgiosa red mundial de agencias de viajes GlobalStar Travel Management, compañía internacional que brinda apoyo y corresponsalía en más de 65 países.</p>
+ 
 
+  <div class="col border bg-ligh m-2">
+    <h2> Misión </h2>
+    <p>Organizamos tu viaje a los lugares más espectaculares que siempre has querido conocer, para que disfrutes en cualquier época del año. En agencia de viajes Amate Travel tenemos el viaje que mejor se adapta a ti.
+       Siempre asesorando al cliente y cuidando hasta el más mínimo detalle para crear el viaje soñado</p>
+    </div>
     
+  <div class="col col border bg-ligh m-2">
+    <h2> Vision </h2>
+    <p>Agencia de Viajes Amate Travel tiene el compromiso de trabajar con sus socios para ofrecer a los clientes el conjunto más completo de soluciones de viaje</p>
+  </div>
+
+</div>
 
 
+
+
+
+<!--Divs informativos-->
+<div class="card-group mt-3" >
+  <div class="card" style="width: 18rem;" id="divservicio">
+  <img class="card-img-top" src="./imgs/serv1.png" alt="Card image cap">
+  <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <a href="#" class="btn btn-primary">Entrar</a>
+
+  </div>
+
+
+
+
+  <div class="card" style="width: 18rem;" id="divservicio">
+  <img class="card-img-top" src="./imgs/serv4.png" alt="Card image cap">
+  <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <a href="#" class="btn btn-primary">Entrar</a>
+
+  </div>
+</div>
 
 @endsection
