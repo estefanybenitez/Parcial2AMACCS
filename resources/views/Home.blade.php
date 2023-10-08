2fa4c891707677439c204ceeceef461459f484af
MINEDUCYT <estefany.benitez22@itca.ed.sv>
2023-10-07 22:27:57 -0600

    portada en home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,31 +1,28 @@
+<link rel="stylesheet" href="./style.css">
 @extends('Layout.app')
 
 @section('content')
 <h1 class="text-center">Home</h1>
-{{-- <hr> --}}
-{{-- insertar contenido  --}}
-<img src="./Agencia2.jpg" alt="agencia" width="100%" height="350px">
-
-<div class="row align-items-start p-3 m-2">
-  <h2> Sobre Nosotros </h2>
-  <p>es una empresa de consultoría y administración de viajes que brinda una completa respuesta a las necesidades de empresas y ejecutivos para viajes de negocios y eventos corporativos.
-    Pertenecemos a la red más sólida y reconocida de agencias de viajes de Latinoamérica L’alianxa Travel Network, con más de 250 puntos de ventas en 95 ciudades, en 18 países. Además, somos parte de la 
-    presitgiosa red mundial de agencias de viajes GlobalStar Travel Management, compañía internacional que brinda apoyo y corresponsalía en más de 65 países.</p>
- 
-
-  <div class="col border bg-ligh m-2">
-    <h2> Misión </h2>
-    <p>Organizamos tu viaje a los lugares más espectaculares que siempre has querido conocer, para que disfrutes en cualquier época del año. En agencia de viajes Amate Travel tenemos el viaje que mejor se adapta a ti.
-       Siempre asesorando al cliente y cuidando hasta el más mínimo detalle para crear el viaje soñado</p>
-    </div>
-    
-  <div class="col col border bg-ligh m-2">
-    <h2> Vision </h2>
-    <p>Agencia de Viajes Amate Travel tiene el compromiso de trabajar con sus socios para ofrecer a los clientes el conjunto más completo de soluciones de viaje</p>
-  </div>
-
+<div class="logo">
+  <img src="./logoITCA.png" alt="logo"  width="50%"
+  height= "250px">
 </div>
 
+
+<div class="row align-items-start p-3 m-2 text-center">
+
+  <div class="col border bg-ligh m-2 ">
+    <h2> Examen Parcial 2 </h2>
+    <p>Tannia Elisa Arevalo Ortiz - 042822</p>
+    <p>Erikson Alexander Garcia Polando -042022</p>
+    <p>Estefany Azucena Benitez Vasquez -142722</p>
+    <p>Daniela Alejandra Sanabria Perez -096122</p>
+    <p>Vanessa Carolina Amaya Martinez -043122</p>
+
+    </div>
+  
+
+</div>
 
 
 
